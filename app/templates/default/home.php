a42389e0bcfcdec4fd62d642fed25a068540a59d
Camille Aubert <aubertcam@gmail.com>
2016-12-12 15:58:37 +0100

    Lien vers site FFH

--- app/templates/default/home.php
+++ app/templates/default/home.php
@@ -2,109 +2,113 @@
 							   'subtitle' => '"Parce que les points administratifs c\'est pour les faibles...et ceux qui font leur licence à temps" ** ']) ?>
 
 <?php $this->start('main_content') ?>
+<div class="sectionDiv">
+  <div class="table-responsive">
+    <table class="table table-hover table-striped">
+        <thead>
+        <tr>
+          <th colspan="2">#</th>
+          <th>Equipe</th>
+          <th>Pts</th>
+          <th>MJ</th>
+          <th>V</th>
+          <th>N</th>
+          <th>D</th>
+          <th>DB</th>
+          <th>BP</th>
+          <th>BC</th>
+        </tr>
+      </thead>
+      <tbody>
 
-<div class="table-responsive">
-  <table class="table table-hover table-striped">
-      <thead>
-      <tr>
-        <th colspan="2">#</th>
-        <th>Equipe</th>
-        <th>Pts</th>
-        <th>MJ</th>
-        <th>V</th>
-        <th>N</th>
-        <th>D</th>
-        <th>Diff</th>
-        <th>Buts+</th>
-        <th>Buts-</th>
-      </tr>
-    </thead>
-    <tbody>
+  	   <?php foreach ($classementEquipes as $Equipe => $data): ?>
 
-	   <?php foreach ($classementEquipes as $Equipe => $data): ?>
+  	      <tr class="<?php if ($data['Pos'] == 1) { echo 'success';}elseif ($data['Pos'] == 9) {echo 'danger';}elseif ($Equipe == 'PARIS SPORT CLUB 5') {echo 'info';} ?>">
+           <td><?php echo $data['Pos']; ?></td>
+            <td>                     
+                (<?php echo $data['PosOfficiel']; ?>)   
+                <?php if($data['Officiel'] === '+'){echo '<i class="fa fa-long-arrow-up" aria-hidden="true"></i>';}
+                      elseif($data['Officiel'] === '-'){echo '<i class="fa fa-long-arrow-down" aria-hidden="true"></i>';}?>               
+            </td>
+  	        <td><?php echo $Equipe; ?></td>
+  	        <td><?php echo $data['Pts']; ?>      (<?php echo $data['PtsOfficiel']; ?>)</td>
+  	        <td><?php echo $data['MJ']; ?></td>
+  	        <td><?php echo $data['V']; ?></td>
+  	        <td><?php echo $data['N']; ?></td>
+  	        <td><?php echo $data['D']; ?></td>
+  	        <td><?php echo $data['Diff']; ?></td>
+  	        <td><?php echo $data['Buts+']; ?></td>
+  	        <td><?php echo $data['Buts-']; ?></td> 
+  	      </tr>
 
-	      <tr class="<?php if ($data['Pos'] == 1) { echo 'success';}elseif ($data['Pos'] == 9) {echo 'danger';}elseif ($Equipe == 'PARIS SPORT CLUB 5') {echo 'info';} ?>">
-         <td><?php echo $data['Pos']; ?></td>
-          <td>                     
-              (<?php echo $data['PosOfficiel']; ?>)   
-              <?php if($data['Officiel'] === '+'){echo '<i class="fa fa-long-arrow-up" aria-hidden="true"></i>';}
-                    elseif($data['Officiel'] === '-'){echo '<i class="fa fa-long-arrow-down" aria-hidden="true"></i>';}?>               
-          </td>
-	        <td><?php echo $Equipe; ?></td>
-	        <td><?php echo $data['Pts']; ?>      (<?php echo $data['PtsOfficiel']; ?>)</td>
-	        <td><?php echo $data['MJ']; ?></td>
-	        <td><?php echo $data['V']; ?></td>
-	        <td><?php echo $data['N']; ?></td>
-	        <td><?php echo $data['D']; ?></td>
-	        <td><?php echo $data['Diff']; ?></td>
-	        <td><?php echo $data['Buts+']; ?></td>
-	        <td><?php echo $data['Buts-']; ?></td> 
-	      </tr>
+  	   <?php endforeach; ?>
+      </tbody>   
+    </table>
+  </div>
 
-	   <?php endforeach; ?>
-    </tbody>   
-  </table>
-</div>
+  <div class="alert alert-success" role="alert">
+  <h5><b>INFORMATIONS:</b></h5>
+    <p>* Modifications par rapport au classement officiel: </br>Les victoires et défaites administratives (20-PE) ne sont pas prises en compte, le vrai score de la rencontre y est substitué. </br>En revanche, les forfaits (20-FO) sont pris en compte de la même manière que sur le classement officiel. </p>
+    <p>** Ce vrai classement ne doit pas nous servir d'excuse pour ne pas faire renouveller notre licence dès juillet :)</p>
+  </div>
 
-<div class="alert alert-success" role="alert">
-<h5><b>INFORMATIONS:</b></h5>
-  <p>* Modifications par rapport au classement officiel: </br>Les victoires et défaites administratives (20-PE) ne sont pas prises en compte, le vrai score de la rencontre y est substitué. </br>En revanche, les forfaits (20-FO) sont pris en compte de la même manière que sur le classement officiel. </p>
-  <p>** Ce vrai classement ne doit pas nous servir d'excuse pour ne pas faire renouveller notre licence dès juillet :)</p>
-</div>
-<div><h2>La liste des matchs :</h2></div>
-
-
-
+</div> 
 
 
-<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+<div class="sectionDiv">
+  <div><h2>La liste des matchs :</h2></div>
 
-  <?php foreach ($rencontresParJournee as $journee => $match): ?>
-    <div class="panel panel-default">
-      <div class="panel-heading" role="tab" id="heading<?php echo $journee;?>">
-        <h4 class="panel-title">
-          <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse<?php echo $journee;?>" aria-expanded="false" aria-controls="collapse<?php echo $journee;?>">
-            <h3>Journée  <?php echo $journee;?></h3>
-          </a>
-        </h4>
-      </div>
-      <div id="collapse<?php echo $journee;?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading<?php echo $journee;?>">
-        <div class="panel-body">
-          <div class="table-responsive">
+  <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 
-            <table class="table table-hover table-striped">
+    <?php foreach ($rencontresParJournee as $journee => $match): ?>
+      <div class="panel panel-default">
+        <div class="panel-heading" role="tab" id="heading<?php echo $journee;?>">
+          <h4 class="panel-title">
+            <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse<?php echo $journee;?>" aria-expanded="false" aria-controls="collapse<?php echo $journee;?>">
+              <h3>Journée  <?php echo $journee;?></h3>
+            </a>
+          </h4>
+        </div>
+        <div id="collapse<?php echo $journee;?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading<?php echo $journee;?>">
+          <div class="panel-body">
+            <div class="table-responsive">
 
-                <thead>
-                <tr>
-                  <th>Domicile</th>
-                  <th colspan="2" class="text-center">Score</th>
-                  <th class="text-right">Exterieur</th>
-                </tr>
-              </thead>
+              <table class="table table-hover table-striped">
 
-              <tbody>
-              <?php foreach ($match as $key => $value): ?>
-                  <tr class="<?php if($value['Nom_equipe_1'] == 'PARIS SPORT CLUB 5' || $value['Nom_equipe_2'] == 'PARIS SPORT CLUB 5'){ echo 'info';} ?>">
-                    <td><?php echo $value['Nom_equipe_1']; ?></td>
-                    <td class="text-right"><?php echo $value['Score_equipe_1']; ?></td>
-                    <td class="text-left"><?php echo $value['Score_equipe_2']; ?></td>
-                    <td class="text-right"><?php echo $value['Nom_equipe_2']; ?></td>
+                  <thead>
+                  <tr>
+                    <th>Domicile</th>
+                    <th colspan="2" class="text-center">Score</th>
+                    <th class="text-right">Exterieur</th>
                   </tr>
-               
-              <?php endforeach; ?>
-              </tbody> 
-                
-            </table>
+                </thead>
 
-          </div><!-- end TableResponsive -->
+                <tbody>
+                <?php foreach ($match as $key => $value): ?>
+                    <tr class="<?php if($value['Nom_equipe_1'] == 'PARIS SPORT CLUB 5' || $value['Nom_equipe_2'] == 'PARIS SPORT CLUB 5'){ echo 'info';} ?>">
+                      <td><?php echo $value['Nom_equipe_1']; ?></td>
+                      <td class="text-right"><?php echo $value['Score_equipe_1']; ?></td>
+                      <td class="text-left"><?php echo $value['Score_equipe_2']; ?></td>
+                      <td class="text-right"><?php echo $value['Nom_equipe_2']; ?></td>
+                    </tr>
+                 
+                <?php endforeach; ?>
+                </tbody> 
+                  
+              </table>
+
+            </div><!-- end TableResponsive -->
+          </div>
         </div>
       </div>
-    </div>
-  <?php endforeach; ?>
+    <?php endforeach; ?>
+  </div>
+
 </div>
 
 
-<div class="row">
+
+<div class="row sectionDiv">
   <div class="col-md-6">
     <div><h2>Meilleure attaque:</h2></div>
     <div class="table-responsive">
--- app/templates/layout.php
+++ app/templates/layout.php
@@ -6,9 +6,33 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
-        <meta name="description" content="">
+
+        <title>Le vrai classement du PSC5</title>
+
+        <meta name="description" content="Cette page contient le classement du championnat de handball niveau : Honneur Departementale +16 masculin - Comité de Paris. Elle a pour but de donner le classement sans prendre en compte les défaites et les victoires administratives, tout en comparant le résultat obtenu avec le classement officiel de la Fédération Française de Handball. De plus elle permet de visualiser le classement des attaques et des défenses. ">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- Chargement des favicons -->
+        <link rel="apple-touch-icon" sizes="57x57" href="<?= $this->assetUrl('favicon/apple-icon-57x57.png') ?>">
+        <link rel="apple-touch-icon" sizes="60x60" href="<?= $this->assetUrl('favicon/apple-icon-60x60.png') ?>">
+        <link rel="apple-touch-icon" sizes="72x72" href="<?= $this->assetUrl('favicon/apple-icon-72x72.png') ?>">
+        <link rel="apple-touch-icon" sizes="76x76" href="<?= $this->assetUrl('favicon/apple-icon-76x76.png') ?>">
+        <link rel="apple-touch-icon" sizes="114x114" href="<?= $this->assetUrl('favicon/apple-icon-114x114.png') ?>">
+        <link rel="apple-touch-icon" sizes="120x120" href="<?= $this->assetUrl('favicon/apple-icon-120x120.png') ?>">
+        <link rel="apple-touch-icon" sizes="144x144" href="<?= $this->assetUrl('favicon/apple-icon-144x144.png') ?>">
+        <link rel="apple-touch-icon" sizes="152x152" href="<?= $this->assetUrl('favicon/apple-icon-152x152.png') ?>">
+        <link rel="apple-touch-icon" sizes="180x180" href="<?= $this->assetUrl('favicon/apple-icon-180x180.png') ?>">
+        <link rel="icon" type="image/png" sizes="192x192"  href="<?= $this->assetUrl('favicon/android-icon-192x192.png') ?>">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?= $this->assetUrl('favicon/favicon-32x32.png') ?>">
+        <link rel="icon" type="image/png" sizes="96x96" href="<?= $this->assetUrl('favicon/favicon-96x96.png') ?>">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?= $this->assetUrl('favicon/favicon-16x16.png') ?>">
+        <link rel="manifest" href="<?= $this->assetUrl('favicon/manifest.json') ?>">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="<?= $this->assetUrl('favicon/ms-icon-144x144.png') ?>">
+        <meta name="theme-color" content="#ffffff">
+
+
+
         <link rel="apple-touch-icon" href="<?= $this->assetUrl('apple-touch-icon.png') ?>">
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
@@ -32,20 +56,27 @@
             <blockquote>
                 <p><em><?= $this->e($subtitle) ?></em></p>
             </blockquote>
-        </div>
           </header>
+        </div>
 
           <section>
             <?= $this->section('main_content') ?>
           </section>
-      </div>    
 
-        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js') ?>"></script>
+        <!-- Panel pour donner le link du site de la FFH -->
+        <div class="panel panel-default">
+          <div class="panel-body">
+            <h3 class="panel-title"><a href="http://www.ff-handball.org/competitions/championnats-departementaux/75-comite-de-paris.html?tx_obladygesthand_pi1[competition_id]=4514&cHash=53dc111887ff438df08f948461e435f3" target="_blank">Le lien vers le classement officiel de la FFH</a></h3>
+          </div>
+        </div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> 
         <script>window.jQuery || document.write('<script src="<?= $this->assetUrl('js/vendor/jquery-1.11.2.min.js') ?>"><\/script>')</script>
 
         <script src="<?= $this->assetUrl('js/vendor/bootstrap.min.js') ?>"></script>
 
         <script src="<?= $this->assetUrl('js/main.js') ?>"></script>
+
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
         <script>
